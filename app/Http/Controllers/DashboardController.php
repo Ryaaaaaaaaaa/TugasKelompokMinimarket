2a007a230a57135112d39e30c955e282950f861a
Siti Maryatul Aminiah <sitimaryatulaminiyah@gmail.com>
2025-01-11 18:35:55 +0700

    memperbaiki

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,16 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Branch;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class DashboardController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Request $request)
     {
-        return view('dashboard');
+        return view('dashboard',['user' => $request->user()]);
     }
 
     /**
